f7f236d8c389b73ab2e83209ef33cec290132248
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2015-03-28 18:37:35 +0100

    Cleanup in GridFinder

--- cluster/grid_finder.py
+++ cluster/grid_finder.py
@@ -8,28 +8,29 @@ class GridFinder(object):
         self.score = score.flat
 
     def find_grid(self):
-        max = 0
+        score_max = 0
         chosen_grid_offset = None
         for grid_offset in [0.125, 0.25, 0.5, 1, 2, 4]:
-            sum = 0
+            score_sum = 0
             hits = 0
             grid_lines = numpy.arange(0, self.score.highestTime, grid_offset)
             for offset in grid_lines:
                 number_of_notes = len(self.score.getElementsByOffset(offset))
-                sum += number_of_notes
+                score_sum += number_of_notes
 
                 if number_of_notes:
                     hits += 1
-            # TODO(zwiener: Use f-measure here.
+            # Ratio of grid lines that contain a chord.
             precision = hits / float(len(grid_lines))
-            recall = sum / float(len(self.score.elements))
+            # Ratio of notes hit by grid line.
+            recall = score_sum / float(len(self.score.elements))
             beta = 0.5
             beta_square = beta * beta
             f_measure = (1 + beta_square) * precision * recall / (beta_square * precision + recall)
             print grid_offset, f_measure, precision, recall
             # Bigger offsets are preferred.
-            if f_measure >= max:
-                max = f_measure
+            if f_measure >= score_max:
+                score_max = f_measure
                 chosen_grid_offset = grid_offset
 
         return chosen_grid_offset
