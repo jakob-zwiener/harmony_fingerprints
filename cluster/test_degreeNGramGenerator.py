1f24950065c1e46ac14576e0b17279067ad2c271
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2015-03-29 14:20:03 +0200

    renamed make_n_grams to to_n_grams

--- cluster/test_degreeNGramGenerator.py
+++ cluster/test_degreeNGramGenerator.py
@@ -11,7 +11,7 @@ class TestDegreeNGramGenerator(TestCase):
         generator = DegreeNGramGenerator(None, None, 3)
         expected_3_grams = [(1, 2, 3), (2, 3, 4), (3, 4, 5), (4, 5, 6), (5, 6, 7), (6, 7, 8), (7, 8, 9)]
 
-        actual_3_grams = generator.make_n_grams([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        actual_3_grams = generator.to_n_grams([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
         self.assertEqual(expected_3_grams, actual_3_grams)
 
